393a74eee602c47081c12d97095ac52f8c210ab6
chenze.li <chenze.li@rea-group.com>
2020-05-12 18:12:25 +0800

    use download file from a s3 and copy it to another

--- src/HelloWorld/index.js
+++ src/HelloWorld/index.js
@@ -1,3 +1,77 @@
-exports.handler = async () => {
-  console.log("hello world");
+// dependencies
+const AWS = require('aws-sdk');
+const util = require('util');
+
+// get reference to S3 client
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context, callback) => {
+
+    // Read options from the event parameter.
+    console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
+    const srcBucket = event.Records[0].s3.bucket.name;
+    // Object key may have spaces or unicode non-ASCII characters.
+    const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    console.log("srcBucket and srcKey are :", srcBucket, srcKey);
+    const dstBucket = "czli-lambda-source";
+    const dstKey    = "copy-" + srcKey;
+
+    // // Infer the image type from the file suffix.
+    // const typeMatch = srcKey.match(/\.([^.]*)$/);
+    // if (!typeMatch) {
+    //     console.log("Could not determine the image type.");
+    //     return;
+    // }
+
+    // // Check that the image type is supported  
+    // const imageType = typeMatch[1].toLowerCase();
+    // if (imageType != "jpg" && imageType != "png") {
+    //     console.log(`Unsupported image type: ${imageType}`);
+    //     return;
+    // }
+
+    // Download the image from the S3 source bucket. 
+
+    try {
+        const params = {
+            Bucket: srcBucket,
+            Key: srcKey
+        };
+        var origimage = await s3.getObject(params).promise();
+
+    } catch (error) {
+        console.log(error);
+        return;
+    }  
+
+    // // set thumbnail width. Resize will set the height automatically to maintain aspect ratio.
+    // const width  = 200;
+
+    // // Use the Sharp module to resize the image and save in a buffer.
+    // try { 
+    //     var buffer = await sharp(origimage.Body).resize(width).toBuffer();
+            
+    // } catch (error) {
+    //     console.log(error);
+    //     return;
+    // } 
+
+    // Upload the thumbnail image to the destination bucket
+    try {
+        const destparams = {
+            Bucket: dstBucket,
+            Key: dstKey,
+            Body: origimage.Body,
+            ContentType: "image"
+        };
+
+        const putResult = await s3.putObject(destparams).promise(); 
+        
+    } catch (error) {
+        console.log(error);
+        return;
+    } 
+        
+    console.log('Successfully download ' + srcBucket + '/' + srcKey +
+        ' and uploaded to ' + dstBucket + '/' + dstKey); 
 };
